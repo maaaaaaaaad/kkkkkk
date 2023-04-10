27e2765284069336f317cdd1f78117cafa95c5a8
maaaaaaaaad <integral5137@gmail.com>
2023-04-10 23:01:43 +0900

    e

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ import (
 func printAlph() {
 	alph := []rune{'a', 'b', 'c', 'd', 'e'}
 	for _, v := range alph {
-		time.Sleep(3000)
+		time.Sleep(300 * time.Millisecond)
 		fmt.Printf("%c\n", v)
 	}
 }
 
 func printNum() {
 	for i := 1; i <= 5; i++ {
-		time.Sleep(3000)
+		time.Sleep(400 * time.Millisecond)
 		fmt.Printf("%d\n", i)
 	}
 }
@@ -24,5 +24,5 @@ func main() {
 	go printAlph()
 	go printNum()
 
-	time.Sleep(3000)
+	time.Sleep(3 * time.Second)
 }
