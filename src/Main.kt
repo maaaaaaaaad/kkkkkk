79c6a2a231abfb523493da7e04879fb88e1f47e0
maaaaaaaaad <integral5137@gmail.com>
2024-07-30 13:17:04 +0900

    e

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,15 @@
 fun main() {
-    val day = 3
+    print("count: ")
+    val day = readlnOrNull() ?: throw Exception("null")
 
     val result = when (day) {
-        1 -> "mon"
-        2 -> "thu"
-        3 -> "wen"
-        4 -> "thu"
-        5 -> "fri"
-        6 -> "sat"
-        7 -> "sun"
+        "1" -> "mon"
+        "2" -> "thu"
+        "3" -> "wen"
+        "4" -> "thu"
+        "5" -> "fri"
+        "6" -> "sat"
+        "7" -> "sun"
         else -> "not"
     }
 
