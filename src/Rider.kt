58a10d6a904832c7e56e1752c3dd55c126d43374
maaaaaaaaad <integral5137@gmail.com>
2024-08-01 20:26:44 +0900

    e

--- src/Rider.kt
+++ src/Rider.kt
@@ -1,5 +1,9 @@
-class Rider(val brand: String, private val model: String, val year: Int) {
-    fun crack(message: String): String {
+interface RiderInterface {
+    fun crack(message: String): String
+}
+
+class Rider(val brand: String, private val model: String, val year: Int) : RiderInterface {
+    override fun crack(message: String): String {
         return "Rider crack ${this.model}, $message!"
     }
 }
