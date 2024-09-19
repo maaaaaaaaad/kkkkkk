f3c93818444af6e254d81f5aed31d034af402583
maaaaaaaaad <integral5137@gmail.com>
2024-09-19 17:18:04 +0900

    e

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,18 @@
-data class Person(val name: String, val age: Int? = null)
+data class Person(val name: String, var age: Int? = null)
 
 fun main(args: Array<String>) {
     val persons = listOf(Person("Alice", 29), Person("Bob", 31), Person("Carol"))
     val maxAgePerson = persons.maxBy { it.age ?: 0 }
     val privateAgePerson = persons.minBy { it.age ?: 0 }
+
     println("Max age set: $maxAgePerson")
     println("Empty age set: $privateAgePerson")
-    fun max(a: Int, b: Int): Int = if (a > b) a else b
-    println(max(1, 2))
+
+    val alice = persons.find { it.name == "Alice" }
+    println("Alice set: $alice")
+    if (alice != null) {
+        alice.age = 20
+    }
+    println("New Alice set: $alice")
 }
 
