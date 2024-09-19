43ac5f4c7addcf49ca8647dcb972978cc9bb8b93
maaaaaaaaad <integral5137@gmail.com>
2024-09-19 14:35:17 +0900

    remove existing modules

--- src/Rider.kt
+++
@@ -1,9 +0,0 @@
-interface RiderInterface {
-    fun crack(message: String): String
-}
-
-class Rider(val brand: String, private val model: String, val year: Int) : RiderInterface {
-    override fun crack(message: String): String {
-        return "Rider crack ${this.model}, $message!"
-    }
-}
