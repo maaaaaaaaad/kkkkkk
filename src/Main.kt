b196f3c7822534765d299a50b27a25c1af7d888e
maaaaaaaaad <integral5137@gmail.com>
2024-09-19 15:19:16 +0900

    e

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,9 @@
 data class Person(val name: String, val age: Int? = null)
 
 fun main(args: Array<String>) {
-    val persons = listOf(Person("Alice", 29), Person("Bob", 31))
-    val oldset = persons.maxBy { it.age ?: 0 }
-    println("Greetings old set: $oldset")
+    val persons = listOf(Person("Alice", 29), Person("Bob", 31), Person("Carol"))
+    val maxAgePerson = persons.maxBy { it.age ?: 0 }
+    val privateAgePerson = persons.minBy { it.age ?: 0 }
+    println("Max age set: $maxAgePerson")
+    println("Empty age set: $privateAgePerson")
 }
