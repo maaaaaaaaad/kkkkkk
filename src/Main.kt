300517c0619277173dcd9cbc46a7f159c80f11b4
maaaaaaaaad <integral5137@gmail.com>
2024-09-22 16:13:49 +0900

    e

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,9 @@
 import datas.Person
 
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
 fun main(args: Array<String>) {
     val persons = listOf(Person("Alice", 29), Person("Bob", 31), Person("Carol"))
     val maxAgePerson = persons.maxBy { it.age ?: 0 }
@@ -12,5 +16,19 @@ fun main(args: Array<String>) {
     println("Alice set: $alice")
     if (alice != null) alice.age = 20
     println("New Alice set: $alice")
+
+    fun eval(expr: Expr): Int {
+        if (expr is Num) {
+            val n = expr as Num
+            return n.value
+        }
+        if (expr is Sum) {
+            return eval(expr.left) + eval(expr.right)
+        }
+        throw IllegalArgumentException("Unknown expression")
+    }
+
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+
 }
 
